8036766cb812e06b521ed0b707f383424a871da7
Sujan Adhikari <sujan.ad6473@gmail.com>
2024-02-06 23:27:48 +0300

    upload file .net_cs

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.WriteLine("Dear Customer!");
+
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscriprion has expired");
+}
+
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration}");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration <=10)
+{
+    Console.WriteLine("Your Subscription will expire soon. Renew now!");
+}
+if (discountPercentage >0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}");
+}
